ca6c651a1957862629298e40f0c78437e957043b
Pathmeswaran <pathmes@kln.ac.lk>
2020-05-30 20:36:04 +0530

    Working

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,38 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
-    
-    
+    mx <- NULL
+    set <- function(y) {
+        x <<- y
+        mx <<- NULL
+    }
+    get <- function()
+        x
+    setmxi <- function(solve)
+        mx  <<- solve
+    getmxi <- function()
+        mx
+    list(
+        set = set,
+        get = get,
+        setmxi = setmxi,
+        getmxi = getmxi
+    )
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    
+    mx <- x$getmxi()
+    if (!is.null(mx)) {
+        message("getting cached data")
+        return(mx)
+    }
+    data <- x$get()
+    mx <- solve(data, ...)
+    x$setmxi(mx)
+    mx
 }
